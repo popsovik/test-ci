8281585a3cd7a970a37e407a4f072660fed4743f
Maliukh Viktor <vmaliukh@lohika.com>
2019-03-07 18:46:34 +0200

    fix2

--- dep.bash
+++ dep.bash
@@ -3,7 +3,11 @@ CURRENT_VERSION=`aws lambda get-alias --function-name test-deploy-first-function
 LATEST_UPLOADED_VERSION=`aws lambda list-versions-by-function --function-name test-deploy-first-function-hello --output json | grep Version | tail -1 | grep -o '[0-9]'`
 S3BUCKET="test-deploy-bucket-inspo"
 
+echo $CURRENT_VERSION
+echo $LATEST_UPLOADED_VERSION
+
 let "TARGET_VERSION =  $LATEST_UPLOADED_VERSION + 1"
+echo $TARGET_VERSION
 #sed 's/##CURRENTVERSION##/'$CURRENT_VERSION'/g; s/##TARGETVERSION##/'$LATEST_UPLOADED_VERSION'/g' appspec-default.yml > appspec.yaml
 sed 's/##CURRENTVERSION##/'\$CURRENT_VERSION'/g; s/##TARGETVERSION##/'\$LATEST_UPLOADED_VERSION'/g' appspec-default.yml > appspec.yaml
 aws s3 cp appspec.yaml s3://$S3BUCKET/test/appspec-$LATEST_UPLOADED_VERSION.yaml
