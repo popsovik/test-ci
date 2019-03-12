063c374125fa447d5c26281dcba0322376fa7115
Maliukh Viktor <vmaliukh@lohika.com>
2019-03-12 15:06:17 +0200

    demo

--- dep.bash
+++ dep.bash
@@ -1,28 +1,19 @@
 #!/bin/bash
-CURRENT_VERSION=`aws lambda get-alias --function-name deploy-testing-first-function-hello --name testAlias --output text | awk '{print $2}'`
-LATEST_UPLOADED_VERSION=`aws lambda list-versions-by-function --function-name deploy-testing-first-function-hello --output json | grep Version | tail -1 | sed s/[^0-9]//g`
+ALIAS="demo"
+LAMBDA_NAME="deploy-testing-CD-demo-hello"
 S3BUCKET="test-deploy-bucket-inspo"
 
-echo $CURRENT_VERSION
-echo $LATEST_UPLOADED_VERSION
+##
+CURRENT_VERSION=`aws lambda get-alias --function-name $LAMBDA_NAME --name $ALIAS --output text | awk '{print $2}'`
+LATEST_UPLOADED_VERSION=`aws lambda list-versions-by-function --function-name $LAMBDA_NAME --output json | grep Version | tail -1 | sed s/[^0-9]//g`
+#LATEST_UPLOADED_VERSION="8"
 
-let "TARGET_VERSION =  $LATEST_UPLOADED_VERSION + 1"
-echo $TARGET_VERSION
-sed 's/##CURRENTVERSION##/'$CURRENT_VERSION'/g; s/##TARGETVERSION##/'$LATEST_UPLOADED_VERSION'/' appspec-default.yml > appspec.yaml
-#sed 's/##CURRENTVERSION##/'\$CURRENT_VERSION'/g; s/##TARGETVERSION##/'\$LATEST_UPLOADED_VERSION'/g' appspec-default.yml > appspec.yaml
-aws s3 cp appspec.yaml s3://$S3BUCKET/test/appspec-$LATEST_UPLOADED_VERSION.yaml
-aws deploy create-deployment --application-name "deploy-testing" --deployment-group test-group --s3-location bucket=$S3BUCKET,bundleType=YAML,key=test/appspec-$LATEST_UPLOADED_VERSION.yaml
+echo Alias $ALIAS
+echo Function Name $LAMBDA_NAME
+echo Current Version $CURRENT_VERSION
+echo Target Version $LATEST_UPLOADED_VERSION
 
-
-
-
-
-
-
-
-
-
-
-
-
-# #aws deploy register-application-revision --application-name "deploy-testing" --s3-location bucket=$S3BUCKET,bundleType=YAML,key=test/appspec-$TARGET_VERSION.yaml
+sed 's/##CURRENTVERSION##/'$CURRENT_VERSION'/; s/##ALIAS##/'$ALIAS'/; s/##LAMBDA_NAME##/'$LAMBDA_NAME'/; s/##TARGETVERSION##/'$LATEST_UPLOADED_VERSION'/' appspec-default.yml > appspec.yaml
+aws s3 cp appspec.yaml s3://$S3BUCKET/test/appspec-$LAMBDA_NAME-$ALIAS-$LATEST_UPLOADED_VERSION.yaml
+aws deploy create-deployment --application-name "deploy-testing" --deployment-group test-group --s3-location bucket=$S3BUCKET,bundleType=YAML,key=test/appspec-$LAMBDA_NAME-$ALIAS-$LATEST_UPLOADED_VERSION.yaml
+aws lambda update-alias --name $ALIAS --function-name $LAMBDA_NAME --function-version $LATEST_UPLOADED_VERSION --routing-config AdditionalVersionWeights={}
--- handler.js
+++ handler.js
@@ -1,7 +1,7 @@
 module.exports.hello = (event, context, callback) => {
   const response = {
     statusCode: 200,
-    body: 'Wait, it is Serverless v1.29.3333!'
+    body: 'EXAMPLE!!!! - Version 2!'
   };
 
   callback(null, response);
