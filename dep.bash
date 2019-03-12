717fab396355dc0e31684e77c9817608b085df3b
Maliukh Viktor <vmaliukh@lohika.com>
2019-03-12 15:27:59 +0200

    new version

--- dep.bash
+++ dep.bash
@@ -16,4 +16,4 @@ echo Target Version $LATEST_UPLOADED_VERSION
 sed 's/##CURRENTVERSION##/'$CURRENT_VERSION'/; s/##ALIAS##/'$ALIAS'/; s/##LAMBDA_NAME##/'$LAMBDA_NAME'/; s/##TARGETVERSION##/'$LATEST_UPLOADED_VERSION'/' appspec-default.yml > appspec.yaml
 aws s3 cp appspec.yaml s3://$S3BUCKET/test/appspec-$LAMBDA_NAME-$ALIAS-$LATEST_UPLOADED_VERSION.yaml
 aws deploy create-deployment --application-name "deploy-testing" --deployment-group test-group --s3-location bucket=$S3BUCKET,bundleType=YAML,key=test/appspec-$LAMBDA_NAME-$ALIAS-$LATEST_UPLOADED_VERSION.yaml
-aws lambda update-alias --name $ALIAS --function-name $LAMBDA_NAME --function-version $LATEST_UPLOADED_VERSION --routing-config AdditionalVersionWeights={}
+#aws lambda update-alias --name $ALIAS --function-name $LAMBDA_NAME --function-version $LATEST_UPLOADED_VERSION --routing-config AdditionalVersionWeights={}
--- handler.js
+++ handler.js
@@ -1,7 +1,7 @@
 module.exports.hello = (event, context, callback) => {
   const response = {
     statusCode: 200,
-    body: 'EXAMPLE!!!! - Version 3!'
+    body: 'EXAMPLE!!!! - Version 4!'
   };
 
   callback(null, response);
