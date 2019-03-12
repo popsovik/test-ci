d04cc67f502016d16bb13c6fb5c73a197e8f626c
Maliukh Viktor <vmaliukh@lohika.com>
2019-03-12 18:32:46 +0200

    test

--- handler.js
+++ handler.js
@@ -1,7 +1,7 @@
 module.exports.hello = (event, context, callback) => {
   const response = {
     statusCode: 200,
-    body: 'EXAMPLE!!!! - Version 6!'
+    body: 'EXAMPLE for script!!!! - Version 7!'
   };
 
   callback(null, response);
