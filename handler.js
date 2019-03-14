c7815a15d30b5e35f2628f246c810b42fd5a2656
Maliukh Viktor <vmaliukh@lohika.com>
2019-03-14 15:44:07 +0200

    egre2

--- handler.js
+++ handler.js
@@ -1,7 +1,7 @@
 module.exports.hello = (event, context, callback) => {
   const response = {
     statusCode: 200,
-    body: 'EXAMPLE new branch2!!!! - Version 23!'
+    body: 'EXAMPLE new branch2!!!! - Version 24!'
   };
 
   callback(null, response);
