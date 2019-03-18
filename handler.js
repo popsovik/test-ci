275d44d8b18c39c75e2332159f4cc39459a56726
Maliukh Viktor <vmaliukh@lohika.com>
2019-03-18 17:24:20 +0200

    check email-3

--- handler.js
+++ handler.js
@@ -1,7 +1,7 @@
 module.exports.hello = (event, context, callback) => {
   const response = {
     statusCode: 200,
-    body: 'EXAMPLE new branch2!!!! - Version 41!'
+    body: 'EXAMPLE new branch2!!!! - Version 42!'
   };
 
   callback(null, response);
